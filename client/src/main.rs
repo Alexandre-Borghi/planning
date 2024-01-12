856fde444463db6ea228ff3ef51a5e1ee045e086
Alexandre Borghi <alexandre@aborghi.fr>
2024-01-12 22:00:30 +0100

    feat(client): show days of a specific month in calendar

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,26 +1,35 @@
 use yew::prelude::*;
+use chrono::{NaiveDate, Weekday, Datelike};
 
 #[function_component]
 fn App() -> Html {
     html! {
         <>
-            <Month></Month>
+            <Month year=2024 month=9></Month>
         </>
     }
 }
 
+#[derive(Properties, PartialEq)]
+struct MonthProps {
+    year: i32,
+    month: u32,
+}
+
 #[function_component]
-fn Month() -> Html {
+fn Month(props: &MonthProps) -> Html {
+    let first_day = NaiveDate::from_ymd_opt(props.year, props.month, 1).unwrap().week(Weekday::Mon).first_day();
+
     html! {
         <div class={classes!("grid", "grid-cols-7", "justify-items-center", "text-center", "max-w-sm", "border-2", "rounded")}>
-            { for (1..=42).map(|number| html! { <Day {number}></Day> }) }
+            { for first_day.iter_days().take(42).map(|date| html! { <Day number={date.day()}></Day> }) }
         </div>
     }
 }
 
 #[derive(Properties, PartialEq)]
 struct DayProps {
-    number: u8,
+    number: u32,
 }
 
 #[function_component]
