fa6672ea8c5d70cd6fef7050cbf2399a73ce6e1a
Alexandre Borghi <alexandre@aborghi.fr>
2024-01-13 19:28:31 +0100

    feat(client): add selectable timeslot buttons

--- client/src/main.rs
+++ client/src/main.rs
@@ -3,13 +3,36 @@ use chrono::{NaiveDate, Weekday, Datelike};
 
 #[function_component]
 fn App() -> Html {
+    let timeslots = use_state(|| vec![String::from("M1"), String::from("M2"), String::from("S1")]);
+    let selected_timeslot = use_state(|| Option::<String>::None);
     let year = use_state(|| 2024);
     let month = use_state(|| 1);
+
+    let onclick = {
+        let selected_timeslot = selected_timeslot.clone();
+        Callback::from(move |id: String| {
+            if selected_timeslot.as_ref().is_some_and(|selected| *selected == id) {
+                selected_timeslot.set(None);
+            } else {
+                selected_timeslot.set(Some(id));
+            }
+        })
+    };
 
     html! {
         <>
             { format!("{:02}/{:04}", *month, *year) }
             <Month year={*year} month={*month}></Month>
+            <div class={classes!("flex", "gap-2")}>
+            { for timeslots.iter().cloned().map(|timeslot| {
+                let onclick = onclick.clone();
+                let timeslot_clone = timeslot.clone();
+                let is_selected = selected_timeslot.as_ref().is_some_and(|selected| *selected == timeslot);
+                html! {
+                    <button class={classes!("px-4", "py-2", "bg-red-500", "rounded-full", is_selected.then_some("ring"))}
+                        onclick={move |_| onclick.emit(timeslot_clone.clone())}>{timeslot}</button>
+            }}) }
+            </div>
         </>
     }
 }
