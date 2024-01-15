748c3f854e1d4217a11931cee112d7344766cc24
Alexandre Borghi <alexandre@aborghi.fr>
2024-01-15 22:15:22 +0100

    feat(server): get calendar api

--- server/src/main.rs
+++ server/src/main.rs
@@ -3,15 +3,23 @@ use actix_web::{get, App, Error, HttpServer};
 
 #[get("/api/timeslots")]
 async fn api_timeslots() -> Result<fs::NamedFile, Error> {
-    let timeslots = fs::NamedFile::open("timeslots.json")?;
+    let timeslots = fs::NamedFile::open("data/timeslots.json")?;
     Ok(timeslots
+        .use_last_modified(true)
+        .set_content_type(mime::APPLICATION_JSON))
+}
+
+#[get("/api/calendar")]
+async fn api_calendar() -> Result<fs::NamedFile, Error> {
+    let calendar = fs::NamedFile::open("data/calendar.json")?;
+    Ok(calendar
         .use_last_modified(true)
         .set_content_type(mime::APPLICATION_JSON))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(api_timeslots))
+    HttpServer::new(|| App::new().service(api_timeslots).service(api_calendar))
         .bind(("127.0.0.1", 8081))?
         .run()
         .await
